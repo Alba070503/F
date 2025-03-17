5cf29b56bfc59dd7c864752402ceed4cd3752d07
Alba070503 <97574964+Alba070503@users.noreply.github.com>
2025-03-17 11:00:25 -0400

    Update Play.js

--- plugins/Play.js
+++ plugins/Play.js
@@ -1,43 +1,37 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-module.exports = {
-  command: ['play'],
-  description: 'Descarga y envía una canción de YouTube en formato MP3.',
-  async handler(m, { conn, args }) {
-    await conn.sendMessage(m.chat, { react: { text: `🎶`, key: m.key }});
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+    await conn.sendMessage(m.chat, { react: { text: `🎶`, key: m.key } });
 
     try {
-      if (!args.length) {
-        return m.reply('🎵 Escribe por favor el nombre de la canción :)...');
-      }
+        if (!text) return m.reply(`🎵 Escribe por favor el nombre de la canción :)...`);
 
-      let query = args.join(' ');
-      let play2 = await fetch(`https://carisys.online/api/pesquisas/youtube?query=${encodeURIComponent(query)}`)
-        .then(res => res.json());
+        let play2 = await fetch(`https://carisys.online/api/pesquisas/youtube?query=${encodeURIComponent(text)}`)
+            .then(res => res.json());
 
-      if (!play2.resultado || !play2.resultado.url) {
-        return m.reply('❌ No se encontró la canción.');
-      }
+        if (!play2.resultado || !play2.resultado.url) return m.reply(`❌ No se encontró la canción.`);
 
-      await conn.sendMessage(m.chat, {
-        audio: { url: `https://carisys.online/api/downloads/youtube/mp3-2?url=${play2.resultado.url}` },
-        fileName: play2.resultado.titulo + '.mpeg',
-        mimetype: "audio/mpeg",
-        contextInfo: {
-          externalAdReply: {
-            title: play2.resultado.titulo,
-            body: `𝐏.𝐀. 𝐙𝐢𝐧 𝐖𝐞𝐛`,
-            mediaType: 1,
-            thumbnailUrl: play2.resultado.imagem,
-            showAdAttribution: true,
-            renderLargerThumbnail: true
-          }
-        }
-      }, { quoted: m });
-
+        await conn.sendMessage(m.chat, {
+            audio: { url: `https://carisys.online/api/downloads/youtube/mp3-2?url=${play2.resultado.url}` },
+            fileName: `${play2.resultado.titulo}.mpeg`,
+            mimetype: "audio/mpeg",
+            contextInfo: {
+                externalAdReply: {
+                    title: play2.resultado.titulo,
+                    body: `𝐏.𝐀. 𝐙𝐢𝐧 𝐖𝐞𝐛`,
+                    mediaType: 1,
+                    reviewType: "PHOTO",
+                    thumbnailUrl: play2.resultado.imagem,
+                    showAdAttribution: true,
+                    renderLargerThumbnail: true
+                }
+            }
+        }, { quoted: m });
     } catch (error) {
-      console.error(error);
-      return m.reply('❌ Hubo un pequeño error :(...');
+        console.error(error);
+        return m.reply(`❌ Hubo un pequeño error al procesar la solicitud.`);
     }
-  }
 };
+
+handler.command = ['playrandom'];
+export default handler;
